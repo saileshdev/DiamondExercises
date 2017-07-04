a2e8861766d3e10cf79b3a7d2959b6051cae6d60
Sailesh Dev <saileshdev@gmail.com>
2017-07-04 15:51:39 +0530

    Add tests to print iscoceles triangle

---
+++ test/com/thoughtworks/PrintIsoscelesTriangleTest.java
@@ -0,0 +1,36 @@
+package com.thoughtworks;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Created by saileshdev on 04/07/2017.
+ */
+public class PrintIsoscelesTriangleTest {
+
+    private IsoscelesTriangle isoscelesTriangle;
+    private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void setUp() throws Exception {
+        this.isoscelesTriangle = new IsoscelesTriangle();
+        this.outputStream = new ByteArrayOutputStream();
+    }
+
+    @Test
+    public void shouldPrintIsoscelesTriangleInConsole(){
+
+        System.setOut(new PrintStream(outputStream));
+        String expected = "  *  \n *** \n*****";
+
+        isoscelesTriangle.drawCenterTriangle(3);
+        assertEquals(outputStream.toString(), expected);
+
+    }
+
+
+}
