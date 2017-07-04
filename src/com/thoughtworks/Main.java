452e0caa21a80dca699495637b22c68f04e5a335
Sailesh Dev <saileshdev@gmail.com>
2017-07-04 16:08:15 +0530

    Add drawDiamond function to Diamond class to print a diamond on console

--- src/com/thoughtworks/Main.java
+++ src/com/thoughtworks/Main.java
@@ -10,5 +10,10 @@ public class Main {
         System.out.println("Question: Draw isosceles triangle to the console(Ex: n=3)");
         isoscelesTriangle.drawCenterTriangle(3);
 
+        Diamond diamond = new Diamond();
+
+        System.out.println("Question: Draw diamond to the console(Ex: n=3)");
+        diamond.drawDiamond(3);
+
     }
 }
